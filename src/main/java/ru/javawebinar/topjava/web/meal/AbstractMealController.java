058453dbbc057362d5c0acdf121467eab475edca
zhukovdima <zhukovdima@list.ru>
2017-11-10 17:23:53 +0600

    AbstarctMealController getBetween method implementation

--- src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
@@ -6,7 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.UsersUtil;
+
+import java.time.LocalTime;
 import java.util.Collection;
+import java.util.List;
+
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 public abstract class AbstractMealController {
@@ -39,5 +46,9 @@ public abstract class AbstractMealController {
 
     public void update(Meal meal) {
 
+    }
+
+    public List<MealWithExceed> getBetween(LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        return MealsUtil.getFilteredWithExceeded(mealService.getAll(AuthorizedUser.id()), startTime, endTime, caloriesPerDay);
     }
 }
