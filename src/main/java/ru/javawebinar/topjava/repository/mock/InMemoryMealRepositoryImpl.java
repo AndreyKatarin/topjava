7994a355ea2ba4ef9fd0f101c0001cd325fede56
zhukovdima <zhukovdima@list.ru>
2017-11-10 16:44:26 +0600

    InMemoryMealRepositoryImpl change methods implementations so as meals depends on userId

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -4,51 +4,48 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class InMemoryMealRepositoryImpl implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.MEALS.forEach(this::save);
+        MealsUtil.MEALS.forEach(m -> save(m, 1));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
+        Objects.requireNonNull(meal);
+
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
+            meal.setUserId(userId);
         }
-        repository.put(meal.getId(), meal);
+        Map<Integer, Meal> meals = repository.computeIfAbsent(userId, m -> new ConcurrentHashMap<>());
+        meals.put(meal.getId(), meal);
         return meal;
     }
 
     @Override
-    public void delete(int id) {
-        repository.keySet().removeIf(k -> k == id);
+    public void delete(int id, int userId) {
+        Map<Integer, Meal> map = repository.get(userId);
+        map.keySet().removeIf(k -> k == id);
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int id, int userId) {
+        Map<Integer, Meal> map = repository.get(userId);
+        return map.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values().stream().sorted((m1, m2) -> m1.getDateTime().compareTo(m2.getDateTime())).collect(Collectors.toList());
-    }
-
-    @Override
-    public Collection<Meal> getAllByUserId(int userId) {
-        List<Meal> list = repository.values().stream().filter(m -> m.getUserId() == userId).collect(Collectors.toList());
-
-        return list.isEmpty() ? Collections.EMPTY_LIST : list;
+    public Collection<Meal> getAll(int userId) {
+        Map<Integer, Meal> map = repository.get(userId);
+        return map.values().stream().sorted((m1, m2) -> m1.getDateTime().compareTo(m2.getDateTime())).collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,13 +1,17 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.List;
 
+@Service
 public class MealServiceImpl implements MealService {
 
+    @Autowired
     private MealRepository repository;
 
     @Override
