99e6f5b6c31f41439c7e749f6b72ccaee3a65170
DimaZhukov <zhukovdima@list.ru>
2017-12-18 18:07:51 +0600

    Taking doGet-method's handlers out of MealServlet to request-mapping methods in JspMealController. Fixing links in meals.jsp

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -2,11 +2,55 @@ package ru.javawebinar.topjava.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.web.meal.AbstractMealController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
 @Controller
-public class JspMealController {
+@RequestMapping(value = "meals")
+public class JspMealController extends AbstractMealController {
 
     @Autowired
-    private MealService mealService;
+    public JspMealController(MealService service) {
+        super(service);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showAll(HttpServletRequest request) {
+        request.setAttribute("meals", super.getAll());
+        return "meals";
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.GET)
+    public String showCreate(HttpServletRequest request) {
+        final Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        request.setAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.GET)
+    public String showUpdate(HttpServletRequest request) {
+        final Meal meal = super.get(getId(request));
+        request.setAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.GET)
+    public String delete(HttpServletRequest request) {
+        int id = getId(request);
+        super.delete(id);
+        return "redirect:/meals";
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
 }
