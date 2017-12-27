5075440168332c1ab33d9a0c5e12856a5afd1e87
zhukovdima <zhukovdima@list.ru>
2017-12-27 10:54:13 +0600

    Creating ResourceControllerTest for testing resourses. Implementing method testCss. Cleaning not used imports

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
-import ru.javawebinar.topjava.model.Meal;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
